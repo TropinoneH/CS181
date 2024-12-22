baf8c47535f9ec8b102e1fd92c8e020dd15cf755
TropinoneH <mspt5871@protonmail.com>
2024-12-23 00:45:00 +0800

    finish q6

--- qlearningAgents.py
+++ qlearningAgents.py
@@ -47,6 +47,7 @@ class QLearningAgent(ReinforcementAgent):
         ReinforcementAgent.__init__(self, **args)
 
         "*** YOUR CODE HERE ***"
+        self.q_values = util.Counter()
 
     def getQValue(self, state, action):
         """
@@ -55,7 +56,7 @@ class QLearningAgent(ReinforcementAgent):
         or the Q node value otherwise
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.q_values[(state, action)]
 
     def computeValueFromQValues(self, state):
         """
@@ -65,7 +66,10 @@ class QLearningAgent(ReinforcementAgent):
         terminal state, you should return a value of 0.0.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        actions = self.getLegalActions(state)
+        if len(actions) == 0:
+            return 0.0
+        return max([self.getQValue(state, act) for act in actions])
 
     def computeActionFromQValues(self, state):
         """
@@ -74,7 +78,11 @@ class QLearningAgent(ReinforcementAgent):
         you should return None.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        actions = self.getLegalActions(state)
+        if len(actions) == 0:
+            return None
+        values = [(self.getQValue(state, act), act) for act in actions]
+        return max(values, key=lambda x: x[0])[1]
 
     def getAction(self, state):
         """
@@ -105,7 +113,9 @@ class QLearningAgent(ReinforcementAgent):
         it will be called on your behalf
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        self.q_values[(state, action)] = (1 - self.alpha) * self.getQValue(state, action) + self.alpha * (
+            reward + self.discount * self.computeValueFromQValues(nextState)
+        )
 
     def getPolicy(self, state):
         return self.computeActionFromQValues(state)
@@ -198,4 +208,3 @@ class BetterExtractor(FeatureExtractor):
         # Add more features here
         "*** YOUR CODE HERE ***"
         return features
-
